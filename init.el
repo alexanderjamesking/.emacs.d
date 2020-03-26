b1fc144689f639a08141a0f671663847a9ce2758
Alex King <alex@alexanderjamesking.com>
2020-03-26 10:10:15 +0100

    flycheck-rust

--- init.el
+++ init.el
@@ -480,12 +480,15 @@
   :init
   (add-hook 'go-mode-hook 'go-eldoc-setup))
 
+(use-package flycheck-rust)
+
 (use-package rust-mode
   :defer t
   :config
   (setq rust-format-on-save t)
   :init
   (add-hook 'rust-mode-hook #'racer-mode)
+  (add-hook 'rust-mode-hook #'flycheck-rust-setup)
   (add-hook 'racer-mode-hook #'eldoc-mode))
 
 
